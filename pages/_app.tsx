40bfff103b641fdd0aeaa435d7f0d33f40f1f1df
CudiLala <augustinemadu9@gmail.com>
2023-02-01 15:31:57 +0100

    created welcome screens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-[22rem] h-[36rem] overflow-auto">
+        <Head>
+          <title>Mola Wallet</title>
+        </Head>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,77 @@
+import molaLogo from "@/public/mola-logo-1.png";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <h1 className="text-lg font-medium p-8">Mola Wallet</h1>;
+  const [introState, setIntroState] = useState<
+    "showLogo" | "showWelcome" | "login"
+  >("showLogo");
+
+  useEffect(() => {
+    if (introState === "showLogo")
+      setTimeout(() => {
+        setIntroState("showWelcome");
+      }, 2000);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="h-full flex justify-center items-center">
+      {introState === "showLogo" && (
+        <Image src={molaLogo} alt="Welcome Image" className="w-36 h-36 flex" />
+      )}
+      {introState === "showWelcome" && <WelcomeScreen />}
+    </div>
+  );
+}
+
+function WelcomeScreen() {
+  const [welcomeState, setWelcomeState] = useState<number>(0);
+
+  const welcomeMessages = [
+    {
+      heading: "Welcome to Mola Vault",
+      body: "Trusted by millons, Mola Wallet is a secure wallet making the world of web3 accessible to all",
+      actionText: "Next",
+    },
+    {
+      heading: "Real time exchange rates",
+      body: "The application allows you to monitor the value of a digital asset like token and ethereum in real time",
+      actionText: "Next",
+    },
+    {
+      heading: "Manage your digital assets",
+      body: "Make transaction to invest, earn, play games and more!",
+      actionText: "Get Started",
+    },
+  ];
+
+  return (
+    <div className="p-6 flex flex-col items-center gap-4">
+      <h1 className="text-center font-semibold text-xl">
+        {welcomeMessages[welcomeState].heading}
+      </h1>
+      <p className="text-center text-gray-700">
+        {welcomeMessages[welcomeState].body}
+      </p>
+      <div className="flex items-center gap-x-3 h-3">
+        {new Array(3).fill(0).map((e, i) => (
+          <button
+            key={i}
+            className={`rounded-full transition-all ${
+              welcomeState === i ? "w-3 h-3 bg-blue-400" : "w-2 h-2 bg-blue-100"
+            }`}
+            onClick={() => setWelcomeState(i)}
+          ></button>
+        ))}
+      </div>
+      <button
+        className="cursor-pointer py-3 px-6 w-40 text-center bg-blue-600 mt-1 leading-none rounded-full font-semibold text-white"
+        onClick={() => (welcomeState < 2 ? setWelcomeState((p) => p + 1) : "")}
+      >
+        {welcomeMessages[welcomeState].actionText}
+      </button>
+    </div>
+  );
 }
