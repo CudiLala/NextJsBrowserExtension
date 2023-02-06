8a5f2445bf09217f040a7675e7cacf4a8d4ce68f
Cudilala <augustinemadu9@gmail.com>
2023-02-06 17:55:58 +0100

    resolved error in token value component

--- page_components/wallet/token_value.tsx
+++ page_components/wallet/token_value.tsx
@@ -1,8 +1,6 @@
 import { AccountContext } from "context/account";
 import { useContext } from "react";
 import { NetworkContext } from "@/context/network";
-import network_styles from "styles/pages/wallet/network_selector.module.css";
-import styles from "styles/pages/wallet/transaction_history.module.css";
 import { networkLogoMap } from "./network_selector";
 
 export default function TokenValue() {
@@ -10,16 +8,16 @@ export default function TokenValue() {
   const [account] = useContext(AccountContext);
 
   return (
-    <div style={{ fontWeight: "600", padding: "3rem" }} className={styles.main}>
+    <div
+      style={{ fontWeight: "600", padding: "3rem" }}
+      className="p-2 bg-white rounded-md"
+    >
       <p style={{ fontSize: "2rem" }}>My Token Value</p>
       <p style={{ fontSize: "2.4rem", padding: "1rem 0" }}>
         ${account.balanceFiat}
       </p>
       <div>
-        <span
-          className={network_styles.network_icon_box}
-          style={{ width: "4.5rem", height: "4.5rem" }}
-        >
+        <span className="bg-white rounded-full w-11 h-11 flex-shrink flex mr-2">
           {networkLogoMap[network.chainName]}
         </span>
       </div>
