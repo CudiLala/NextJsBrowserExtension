2b67a6b42c393dec2b3b444c163a023505c202d2
Cudilala <augustinemadu9@gmail.com>
2023-02-17 18:37:32 +0100

    ..

--- pages/wallet/import/keystore.tsx
+++ pages/wallet/import/keystore.tsx
@@ -1,3 +1,14 @@
+import BackButton from "@/components/button/back";
+
 export function ImportWithKesystore() {
-  return <></>;
+  return (
+    <div className="flex flex-col">
+      <div className="py-2 px-4 sticky top-0 z-20 border-b border-neutral-300 bg-white">
+        <h1 className="text-base text-center font-medium relative">
+          <BackButton />
+          Create With Keystore
+        </h1>
+      </div>
+    </div>
+  );
 }
